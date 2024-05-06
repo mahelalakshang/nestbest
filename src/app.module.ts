038c98b6604d1b212a48824ed3380f2e45443f79
Mahela Lakshan <mahela@mediio.net>
2024-05-06 13:07:10 +0530

    validation

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,8 @@
 import { Module } from '@nestjs/common';
+import { UsersModule } from './domain/users/users.module';
+import { CommonModule } from './common/common.module';
 
 @Module({
-  imports: [],
+  imports: [UsersModule, CommonModule],
 })
 export class AppModule {}
